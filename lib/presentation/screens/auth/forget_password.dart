53f09ba29397b02c7887de8832c6a92c52230da9
AmmarMatarieh2000 <ammarmatarieh2@gmail.com>
2025-11-13 15:41:43 +0300

    update

--- lib/presentation/screens/auth/forget_password.dart
+++ lib/presentation/screens/auth/forget_password.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:khsomati/presentation/widget/forget_pass_widget.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
   const ForgetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Forget Password"),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: ForgetPasswordWidget(),
+    );
   }
 }
--- lib/presentation/widget/forget_pass_widget.dart
+++ lib/presentation/widget/forget_pass_widget.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ForgetPasswordWidget extends StatefulWidget {
+  const ForgetPasswordWidget({super.key});
+
+  @override
+  State<ForgetPasswordWidget> createState() => _ForgetPasswordWidgetState();
+}
+
+class _ForgetPasswordWidgetState extends State<ForgetPasswordWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
