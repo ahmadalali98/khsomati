d13a3b4863f1a08e1f76111f92a42d31bed1e8c0
MKmasri2002 <almasrimahmood98@gmail.com>
2025-11-15 02:00:09 +0300

    w

---
+++ lib/constants/app_size.dart
@@ -0,0 +1,8 @@
+class AppSize {
+  static double width = 0;
+  static double height = 0;
+  static void setSize({required double newWidth, required double newHeight}) {
+    width = newWidth;
+    height = newHeight;
+  }
+}
